6bf780296438c48de4eb7d49d38c31ce174d149c
Wayne Camara <waynecamara@biud.com.br>
2023-07-05 16:44:03 -0300

    fix: Analise do metodo fetch - http

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import './style.css';
 
+//https://sujeitoprogramador.com/rn-api/?api=posts
 function App() {
-  const [name, setName] = useState("");
+  const [nutri, setNutri] = useState([]);
+
+  useEffect(() => {
+    function loadApi() {
+      let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
+      fetch(url)
+
+        .then((p) => p.json())
+        .then((json) => {
+          setNutri(json)
+        })
+    }
+    loadApi()
+  }, [])
+
+
   return (
-    <div className='first-section'>
-      <h1>TESTE</h1>
+    <div className='blog-container'>
+      <header>
+        <h1>React Nutri</h1>
+      </header>
+      {nutri.map((item) => {
+        return (
+          <article key={item.id} className='article-content'>
+            <strong className="article-description">
+              {item.titulo}
+            </strong>
+            <img className='img-source' src={item.capa} alt={item.title} />
+            <p className='subtitulo-content'>{item.subtitulo}</p>
+            <p className='subtitulo-content'>Categoria: {item.categoria}</p>
+            <a className='btn-view-more'> Acessar </a>
+          </article>
+        )
+      })}
     </div>
   )
 }

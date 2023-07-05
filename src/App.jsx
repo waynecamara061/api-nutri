c66bd513042d8d8f3278dfb920432528b4051641
Wayne Camara <waynecamara@biud.com.br>
2023-07-05 15:50:04 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+function App() {
+  const [name, setName] = useState("");
+  return (
+    <div className='first-section'>
+      <h1>TESTE</h1>
+    </div>
+  )
+}
+
+export default App;
